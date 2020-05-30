426fc06c6ec5ef6e32c3b4cfb543b873d722b407
Maheshkumar <mtmaheshsairam84@gmail.com>
2020-05-30 14:39:20 +0530

    Fixed : added views/index.html

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var dotenv = require('dotenv')
+var bodyparser = require('body-parser')
+var path = require('path')
 var app = express()
 
 dotenv.config(process.cwd(), '.env')
@@ -13,6 +15,13 @@ try {
 } catch (error) {
   console.log(error)
 }
+
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Server runing on ', process.env.PORT || 3000)
